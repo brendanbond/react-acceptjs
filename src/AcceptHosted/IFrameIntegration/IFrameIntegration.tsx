29567701d35e855539872b9e761437bce97aa3e0
Brendan Bond <brendanbond@gmail.com>
2021-11-22 10:52:18 -0600

    iframe integration now has a compound component api

--- src/AcceptHosted/IFrameIntegration/IFrameIntegration.tsx
+++ src/AcceptHosted/IFrameIntegration/IFrameIntegration.tsx
@@ -1,29 +1,37 @@
-import * as React from 'react';
+import React from 'react';
 
 import {
   AcceptHostedTransactionResponse,
   AcceptHostedIFrameIntegrationProps,
+  CompoundComponentCommonProps,
+  CompoundComponentWithChildrenProps,
+  IFrameIntegrationContext,
 } from '../../types';
 import { parseQueryString } from './helpers';
-import {
-  authorizeNetPopupScreenStyle,
-  authorizeNetPopupBottomStyle,
-  authorizeNetPopupCloseLinkStyle,
-  authorizeNetPopupCloseStyle,
-  authorizeNetPopupInnerStyle,
-  authorizeNetPopupLogoStyle,
-  authorizeNetPopupOuterStyle,
-  authorizeNetPopupStyle,
-  authorizeNetPopupTopStyle,
-} from './styles';
+import { defaultBackdropStyle, defaultContainerStyle } from './styles';
+import useWindowSize from '../../hooks/useWindowSize';
 
-const IFrameIntegration = ({
+const IFrameIntegrationContext =
+  React.createContext<IFrameIntegrationContext | null>(null);
+
+export const useIFrameIntegrationContext = () => {
+  const context = React.useContext(IFrameIntegrationContext);
+  if (!context) {
+    throw new Error(
+      `AcceptHosted compound components cannot be rendered outside the AcceptHosted component`
+    );
+  }
+  return context;
+};
+
+export const IFrameIntegration = ({
   formToken,
   postUrl,
   onTransactionResponse,
   onCancel,
   onSuccessfulSave,
   onResize,
+  children,
 }: AcceptHostedIFrameIntegrationProps) => {
   const [popupIsShown, setPopupIsShown] = React.useState(false);
   const popupFormRef = React.useRef<HTMLFormElement>(null);
@@ -38,31 +46,43 @@ const IFrameIntegration = ({
     setPopupIsShown(false);
   };
 
-  const centerPopup = () => {
-    if (popupRef.current) {
-      popupRef.current.style.left = '50%';
-      popupRef.current.style.top = '50%';
-      const left = -Math.floor(popupRef.current.clientWidth / 2);
-      const top = -Math.floor(popupRef.current.clientHeight / 2);
-      popupRef.current.style.marginLeft = `${left.toString()}px`;
-      popupRef.current.style.marginTop = `${top.toString()}px`;
-      popupRef.current.style.zIndex = '2';
-      if (popupRef.current.offsetLeft < 16) {
-        popupRef.current.style.left = '16px';
-        popupRef.current.style.marginLeft = '0px';
-      }
-      if (popupRef.current.offsetTop < 16) {
-        popupRef.current.style.top = '16px';
-        popupRef.current.style.marginTop = '0px';
-      }
-    }
+  const contextValue = {
+    popupIsShown,
+    popupFormRef,
+    popupRef,
+    iFrameRef,
+    formToken,
+    postUrl,
+    handleShowPopup,
+    handleClosePopup,
   };
 
-  React.useEffect(() => {
-    if (popupIsShown) {
-      centerPopup();
-    }
-  }, [popupIsShown]);
+  // const centerPopup = () => {
+  //   if (popupRef.current) {
+  //     popupRef.current.style.left = '50%';
+  //     popupRef.current.style.top = '50%';
+  //     const left = -Math.floor(popupRef.current.clientWidth / 2);
+  //     const top = -Math.floor(popupRef.current.clientHeight / 2);
+  //     popupRef.current.style.marginLeft = `${left.toString()}px`;
+  //     popupRef.current.style.marginTop = `${top.toString()}px`;
+  //     popupRef.current.style.zIndex = '2';
+  //     if (popupRef.current.offsetLeft < 16) {
+  //       popupRef.current.style.left = '16px';
+  //       popupRef.current.style.right = '16px';
+  //       popupRef.current.style.marginLeft = '0px';
+  //     }
+  //     if (popupRef.current.offsetTop < 16) {
+  //       popupRef.current.style.top = '16px';
+  //       popupRef.current.style.marginTop = '0px';
+  //     }
+  //   }
+  // };
+
+  // React.useEffect(() => {
+  //   if (popupIsShown) {
+  //     centerPopup();
+  //   }
+  // }, [popupIsShown]);
 
   React.useEffect(() => {
     if (!window.AuthorizeNetIFrame) {
@@ -97,10 +117,11 @@ const IFrameIntegration = ({
               if (width && height) {
                 const w = parseInt(width);
                 const h = parseInt(height);
-                if (iFrameRef.current) {
-                  iFrameRef.current.style.width = `${w.toString()}px`;
+                if (iFrameRef.current && popupRef.current) {
+                  iFrameRef.current.style.width = '100%';
                   iFrameRef.current.style.height = `${h.toString()}px`;
-                  centerPopup();
+                  popupRef.current.style.height = `${h.toString()}px`;
+                  // centerPopup();
                 }
                 if (onResize) onResize(w, h);
               }
@@ -114,82 +135,116 @@ const IFrameIntegration = ({
   });
 
   return (
-    <div>
-      <form
-        method="post"
-        action={postUrl}
-        id="formAuthorizeNetPopup"
-        name="formAuthorizeNetPopup"
-        target="iframeAuthorizeNet"
-        ref={popupFormRef}
-        onSubmit={handleShowPopup}
-      >
-        <input type="hidden" id="popupToken" name="token" value={formToken} />
-        <div>
-          Trigger Accept Transaction
-          <button disabled={popupIsShown}>Open AuthorizeNetPopup</button>
-        </div>
-      </form>
-      <div
-        id="divAuthorizeNetPopupScreen"
-        style={{
-          ...authorizeNetPopupScreenStyle,
-          display: popupIsShown ? '' : 'none',
-        }}
-      />
-      <div
-        ref={popupRef}
-        style={{
-          ...authorizeNetPopupStyle,
-          display: popupIsShown ? '' : 'none',
-        }}
-      >
-        <div
-          className="AuthorizeNetPopupOuter"
-          style={authorizeNetPopupOuterStyle}
-        >
-          <div
-            className="AuthorizeNetPopupTop"
-            style={authorizeNetPopupTopStyle}
-          >
-            <div
-              className="AuthorizeNetPopupClose"
-              style={authorizeNetPopupCloseStyle}
-            >
-              <a
-                onClick={handleClosePopup}
-                title="Close"
-                style={authorizeNetPopupCloseLinkStyle}
-              >
-                {' '}
-              </a>
-            </div>
-          </div>
-          <div
-            className="AuthorizeNetPopupInner"
-            style={authorizeNetPopupInnerStyle}
-          >
-            <iframe
-              name="iframeAuthorizeNet"
-              id="iframeAuthorizeNet"
-              frameBorder="0"
-              ref={iFrameRef}
-            ></iframe>
-          </div>
-          <div
-            className="AuthorizeNetPopupBottom"
-            style={authorizeNetPopupBottomStyle}
-          >
-            <div
-              className="AuthorizeNetPopupLogo"
-              style={authorizeNetPopupLogoStyle}
-              title="Powered by Authorize.net"
-            ></div>
-          </div>
-        </div>
-      </div>
+    <IFrameIntegrationContext.Provider value={contextValue}>
+      {children}
+    </IFrameIntegrationContext.Provider>
+  );
+};
+
+export const IFrameIntegrationButton = ({
+  children,
+  className,
+}: CompoundComponentWithChildrenProps) => {
+  const { postUrl, handleShowPopup, formToken, popupIsShown } =
+    useIFrameIntegrationContext();
+  return (
+    <form
+      method="post"
+      action={postUrl}
+      id="formAuthorizeNetPopup"
+      name="formAuthorizeNetPopup"
+      target="iframeAuthorizeNet"
+      onSubmit={handleShowPopup}
+    >
+      <input type="hidden" id="popupToken" name="token" value={formToken} />
+      <button disabled={popupIsShown} className={className}>
+        {children}
+      </button>
+    </form>
+  );
+};
+
+export const IFrameContainer = ({
+  children,
+  className,
+  style,
+}: CompoundComponentWithChildrenProps) => {
+  const { popupIsShown, popupRef } = useIFrameIntegrationContext();
+  const windowSize = useWindowSize();
+  const [offsetWidth, setOffsetWidth] = React.useState(0);
+  const [offsetHeight, setOffsetHeight] = React.useState(0);
+
+  // React.useEffect(() => {
+  //   if (popupIsShown) {
+  //     debugger;
+  //   }
+  // }, [popupIsShown]);
+
+  React.useEffect(() => {
+    setOffsetWidth(Math.floor((popupRef.current?.clientWidth || 0) / 2));
+    setOffsetHeight(Math.floor((popupRef.current?.clientHeight || 0) / 2));
+  }, [popupRef]);
+
+  return (
+    <div
+      className={className}
+      ref={popupRef}
+      style={
+        className
+          ? { display: popupIsShown ? '' : 'none' }
+          : {
+              ...defaultContainerStyle,
+              ...style,
+              display: popupIsShown ? '' : 'none',
+              width: windowSize.width
+                ? windowSize.width > 576
+                  ? '70%'
+                  : '300px'
+                : '300px',
+              height: '90%',
+              marginLeft: `-${offsetWidth}px`,
+              marginTop: `-${offsetHeight}px`,
+            }
+      }
+    >
+      {children}
     </div>
   );
 };
 
-export default IFrameIntegration;
+export const IFrameBackdrop = ({
+  className,
+  style,
+}: CompoundComponentCommonProps) => {
+  const { popupIsShown } = useIFrameIntegrationContext();
+
+  return (
+    <div
+      className={className}
+      style={
+        className
+          ? { display: popupIsShown ? '' : 'none' }
+          : {
+              ...defaultBackdropStyle,
+              ...style,
+              display: popupIsShown ? '' : 'none',
+            }
+      }
+    />
+  );
+};
+
+export const IFrame = ({ className, style }: CompoundComponentCommonProps) => {
+  return (
+    <iframe
+      name="iframeAuthorizeNet"
+      id="iframeAuthorizeNet"
+      frameBorder="0"
+      scrolling="no"
+      width="100%"
+      height="100%"
+      className={className}
+      style={className ? {} : style}
+    ></iframe>
+  );
+};
