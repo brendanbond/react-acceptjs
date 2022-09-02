ad54fe3f75336fdc4b4bc88f261beb3a117c08c2
Brendan Bond <brendanbond@gmail.com>
2022-09-02 10:26:53 -0500

    upgrade to new createRoot API from React 18

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './App.css';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
