de594d3f07004da71d21d20b384af52602e13e0c
Brendan Bond <brendanbond@gmail.com>
2022-09-02 17:35:52 -0500

    add rollup; update module resolution

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+import typescript from '@rollup/plugin-typescript';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+import packageJson from './package.json';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+      name: 'react-acceptjs',
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [peerDepsExternal(), typescript()],
+};
